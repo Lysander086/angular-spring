239be4faac5fde23f721e5ddfd19ff30eed9b533
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-03-27 15:52:18 +0800

    ing 6. get data from a server

--- angular-heros/src/app/heroes/heroes.component.ts
+++ angular-heros/src/app/heroes/heroes.component.ts
@@ -1,58 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { TESTING } from '../config';
-
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
 })
+
 export class HeroesComponent implements OnInit {
-  // In TypeScript, each member is public by default.
-  // default: []
   heroes: Hero[];
 
-  // strictNullChecks
-  selectedHero?: Hero;
-
-  /**
-   * @narration:
-   *  (a)Resource
-   *  private HeroService heroService;
-   * @param heroService
-   * @param messageService
-   */
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {
-    this.setDefaultValues();
+  constructor(private heroService: HeroService) { }
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
   }
-
-  setDefaultValues() {
-    this.heroes = [];
-  }
-
   ngOnInit() {
     this.getHeroes();
-    if (TESTING) this.selectedHero = this.heroes[0];
   }
-
-  onSelect(hero: Hero): void {
-    if (this.selectedHero === hero) {
-      this.selectedHero = undefined;
-    } else {
-      this.selectedHero = hero;
-      this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-    }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
   }
-
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe((heroes) => {
-      this.heroes = heroes;
-    });
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
