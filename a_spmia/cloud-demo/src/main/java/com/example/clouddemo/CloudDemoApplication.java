aa29ebf488882f62f6b8773c27f38e840f63f682
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-03-29 23:05:25 +0800

    ch 2

--- a_spmia/cloud-demo/src/main/java/com/example/clouddemo/CloudDemoApplication.java
+++ a_spmia/cloud-demo/src/main/java/com/example/clouddemo/CloudDemoApplication.java
@@ -2,13 +2,23 @@ package com.example.clouddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-
+@RestController
+@RequestMapping(value = "hi")
+@EnableCircuitBreaker
+@EnableEurekaClient
 public class CloudDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudDemoApplication.class, args);
     }
+
+
+
 
 }
