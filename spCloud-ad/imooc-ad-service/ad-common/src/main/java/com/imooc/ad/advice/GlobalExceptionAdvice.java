4045db2ea705ad4b38b58d1da384e334b9832fe4
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-04-08 00:01:56 +0800

    c4 complete

---
+++ spCloud-ad/imooc-ad-service/ad-common/src/main/java/com/imooc/ad/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,25 @@
+package com.imooc.ad.advice;
+
+import com.imooc.ad.exception.AdException;
+import com.imooc.ad.vo.CommonResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 
+ */
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+
+    @ExceptionHandler(value = AdException.class)
+    /* An HTTP servlet is a special type of servlet that handles an HTTP request and provides an HTTP response, usually in the form of an HTML page */
+    public CommonResponse<String> handlerAdException(HttpServletRequest req,
+                                                     AdException ex) {
+        CommonResponse<String> response = new CommonResponse<>(-1,
+                "business error");
+        response.setData(ex.getMessage());
+        return response;
+    }
+}
