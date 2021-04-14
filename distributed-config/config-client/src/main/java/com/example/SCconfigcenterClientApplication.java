f4304295458cbc8282be021287415aa2c163b7e0
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-04-14 14:49:43 +0800

    ing distributed config

---
+++ distributed-config/config-client/src/main/java/com/example/SCconfigcenterClientApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class SCconfigcenterClientApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(SCconfigcenterClientApplication.class) ;
+    }
+}
