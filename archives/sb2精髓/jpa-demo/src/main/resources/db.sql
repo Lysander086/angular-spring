6e9c1f3a82a101d3126370137ab72df6f61a59fc
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-06-15 10:47:22 +0800

    update db for mac

--- archives/sb2精髓/jpa-demo/src/main/resources/db.sql
+++ archives/sb2精髓/jpa-demo/src/main/resources/db.sql
@@ -1,15 +1,15 @@
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) COLLATE utf8_bin DEFAULT NULL COMMENT '名称',
-  `department_id` int(11) DEFAULT NULL,
-  `create_time` date DEFAULT NULL COMMENT '创建时间',
-  PRIMARY KEY (`id`)
+                        `id` int(11) NOT NULL AUTO_INCREMENT,
+                        `name` varchar(45) COLLATE utf8_bin DEFAULT NULL COMMENT '名称',
+                        `department_id` int(11) DEFAULT NULL,
+                        `create_time` date DEFAULT NULL COMMENT '创建时间',
+                        PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 CREATE TABLE `department` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`id`)
+                              `id` int(11) NOT NULL AUTO_INCREMENT,
+                              `name` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+                              PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
