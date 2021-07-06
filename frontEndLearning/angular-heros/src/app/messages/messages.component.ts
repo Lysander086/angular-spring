7a672b88fbb2bd09153af8b233eb8dd348907f02
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-07-06 18:14:02 +0800

    no msg

--- frontEndLearning/angular-heros/src/app/messages/messages.component.ts
+++ frontEndLearning/angular-heros/src/app/messages/messages.component.ts
@@ -10,7 +10,15 @@ export class MessagesComponent implements OnInit {
 
   constructor(public messageService: MessageService) {}
 
+  // markz: play ts here
   ngOnInit() {
+    function printCoordinate(pt: { x: number; y?: number }) {
+      console.log('The coordinate\'s x value is ' + pt.x);
+      console.log('The coordinate\'s y value is ' + pt.y);
+    }
+
+    printCoordinate({ x: 1 });
+
   }
 
 }
