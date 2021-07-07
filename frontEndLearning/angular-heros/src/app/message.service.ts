6c2045d3c58c1f27261943820ad94f7aae423fba
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-07-07 10:56:49 +0800

    test

--- frontEndLearning/angular-heros/src/app/message.service.ts
+++ frontEndLearning/angular-heros/src/app/message.service.ts
@@ -12,3 +12,6 @@ export class MessageService {
     this.messages = [];
   }
 }
+
+
+
--- frontEndLearning/angular-heros/src/app/messages/messages.component.ts
+++ frontEndLearning/angular-heros/src/app/messages/messages.component.ts
@@ -1,6 +1,200 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from '../message.service';
 
+
+const CALCULATOR_SERVICE_FIX = 'com.richdataco.cde.decision.function.';
+
+const CALCULATOR_SERVICE = {
+  /*calculatorService: {
+      calculateEligibleLoanOffer: {
+          name: `${CALCULATOR_SERVICE_FIX}CalculatorService#calculateEligibleLoanOffer`,
+          returnType: 'object',
+          type: 'function',
+          parameters: [
+              {
+                  type: 'variable',
+                  key: '',
+                  dataType: '',
+              }, {
+                  type: 'variable',
+                  key: '',
+                  dataType: '',
+              }, {
+                  type: 'variable',
+                  key: '',
+                  dataType: '',
+              }, {
+                  type: 'variable',
+                  key: '',
+                  dataType: '',
+              }, {
+                  type: 'function',
+                  name: 'com.richdataco.cde.decision.function.RuleDecisionTableService#getParameter',
+                  returnType: 'object',
+                  parameters: [
+                      {type: 'input', value: 'PT007', dataType: 'string'}
+                  ]
+              }, {
+                  type: 'function',
+                  name: 'com.richdataco.cde.decision.function.RuleDecisionTableService#getParameter',
+                  returnType: 'object',
+                  parameters: [
+                      {type: 'input', value: 'PT007', dataType: 'string'}
+                  ]
+              }, {
+                  type: 'function',
+                  name: 'com.richdataco.cde.decision.function.RuleDecisionTableService#getParameter',
+                  returnType: 'object',
+                  parameters: [
+                      {type: 'input', value: 'PT007', dataType: 'string'}
+                  ]
+              }, {
+                  type: 'math',
+                  returnType: 'integer',
+                  expression: '',
+                  parameters: []
+              }, {
+                  type: 'function',
+                  name: 'com.richdataco.cde.decision.function.RuleDecisionTableService#getParameter',
+                  returnType: 'object',
+                  parameters: [
+                      {type: 'input', value: 'PT007', dataType: 'string'}
+                  ]
+              }, {
+                  type: 'math',
+                  returnType: 'integer',
+                  expression: '',
+                  parameters: []
+              }, {
+                  name: 'output',
+                  type: 'bean',
+                  properties: [
+                      {type: 'property', name: '', value: {type: 'input', value: 'maxAmount', dataType: 'string'}},
+                      {type: 'property', name: '', value: {type: 'input', value: 'instalmentAmount', dataType: 'string'}},
+                      {type: 'property', name: '', value: {type: 'input', value: 'loanTermRepayments', dataType: 'string'}}
+                  ]
+              }
+          ]
+      },
+      getInstalmentAmountByRepayments: {
+          name: `${CALCULATOR_SERVICE_FIX}CalculatorService#getInstalmentAmountByRepayments`,
+          returnType: 'big-decimal',
+          type: 'function',
+          parameters: [
+              {
+                  type: 'variable',
+                  key: '',
+                  dataType: '',
+              }, {
+                  type: 'function',
+                  name: 'com.richdataco.cde.decision.function.RuleDecisionTableService#getParameter',
+                  returnType: 'object',
+                  parameters: [
+                      {type: 'input', value: 'PT009', dataType: 'string'}
+                  ]
+              }, {
+                  type: 'variable',
+                  key: '',
+                  dataType: '',
+              }, {
+                  type: 'variable',
+                  key: '',
+                  dataType: '',
+              }
+          ]
+      }
+
+  },*/
+  merchantLendingCalculator: {
+    calculate: {
+      name: `${CALCULATOR_SERVICE_FIX}MerchantLendingCalculator#calculate`,
+      returnType: 'object',
+      type: 'function',
+      parameters: [
+        {
+          type: 'variable',
+          key: '',
+          dataType: ''
+        }, {
+          type: 'variable',
+          key: '',
+          dataType: ''
+        }, {
+          type: 'variable',
+          key: '',
+          dataType: ''
+        }, {
+          type: 'variable',
+          key: '',
+          dataType: ''
+        }, {
+          name: 'output',
+          type: 'bean',
+          properties: [
+            { type: 'property', name: '', value: { type: 'input', value: 'loanAmount', dataType: 'string' } },
+            { type: 'property', name: '', value: { type: 'input', value: 'feesAmount', dataType: 'string' } },
+            {
+              type: 'property',
+              name: '',
+              value: { type: 'input', value: 'estimatedRepaymentTerm', dataType: 'string' }
+            },
+            {
+              type: 'property',
+              name: '',
+              value: { type: 'input', value: 'estimatedRepaymentAmount', dataType: 'string' }
+            },
+            { type: 'property', name: '', value: { type: 'input', value: 'totalRepaymentAmount', dataType: 'string' } }
+          ]
+        }
+      ]
+    }
+  },
+  methodBean: {
+    getAge: {
+      name: `${CALCULATOR_SERVICE_FIX}MethodBean#getAge`,
+      returnType: 'integer',
+      type: 'function',
+      parameters: [
+        {
+          type: 'variable',
+          key: '',
+          dataType: ''
+        }
+      ]
+    },
+    getNumberOfMonths: {
+      name: `${CALCULATOR_SERVICE_FIX}MethodBean#getNumberOfMonths`,
+      returnType: 'integer',
+      type: 'function',
+      parameters: [
+        {
+          type: 'variable',
+          key: '',
+          dataType: ''
+        },
+        {
+          type: 'variable',
+          key: '',
+          dataType: ''
+        }
+      ]
+    },
+    getHourOfDay: {
+      name: `${CALCULATOR_SERVICE_FIX}MethodBean#getHourOfDay`,
+      returnType: 'integer',
+      type: 'function',
+      parameters: [
+        {
+          type: 'variable',
+          key: '',
+          dataType: ''
+        }
+      ]
+    }
+  }
+};
+
+
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
@@ -8,16 +202,19 @@ import { MessageService } from '../message.service';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor(public messageService: MessageService) {}
+  constructor(public messageService: MessageService) {
+  }
 
   // markz: play ts here
   ngOnInit() {
-    function printCoordinate(pt: { x: number; y?: number }) {
-      console.log('The coordinate\'s x value is ' + pt.x);
-      console.log('The coordinate\'s y value is ' + pt.y);
-    }
+    console.log(Object.keys(CALCULATOR_SERVICE), Object.keys(CALCULATOR_SERVICE).length);
+    Object.keys(CALCULATOR_SERVICE).forEach(key => {
+      if (CALCULATOR_SERVICE.hasOwnProperty(key)) {
+        console.log(true);
+      }
 
-    printCoordinate({ x: 1 });
+    });
+
 
   }
 
